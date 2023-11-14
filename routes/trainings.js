6090328a43c7af202d06c6754b23220a2ef588d1
markneu22 <markneu22@gmail.com>
2023-11-14 09:38:04 +0100

    class over

---
+++ routes/trainings.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+ getTraining,
+ getTrainings,
+ createTraining,
+ updateTraining,
+ deleteTraining,
+} = require("../controllers/trainings");
+const router = express.Router();
+router.route("/").get(getTrainings).post(createTraining);
+router
+ .route("/:id")
+ .get(getTraining)
+ .put(updateTraining)
+ .delete(deleteTraining);
+module.exports = router;
