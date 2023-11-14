da902fdc6af155a4e255acb4debfcd3dedd00f63
markneu22 <markneu22@gmail.com>
2023-11-14 09:06:13 +0100

    A képzések végpontjai elkészültek a trainings.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
+const trainings = require('./routes/trainings')
 app.listen(process.env.PORT,console.log(`Server running on port ${process.env.PORT}`));
