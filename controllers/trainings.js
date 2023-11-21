5a635f3122dc6c573d72d35012337059bcba3dd5
markneu22 <markneu22@gmail.com>
2023-11-21 09:31:06 +0100

    GetByID does not work yet

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -7,15 +7,21 @@ exports.getTrainings = async(req, res, next) => {
           const trainings = await Training.find()
           res.status(200).json({ success: true, msg: "Show all trainings", data: trainings });
      } catch (error) {
-          res.status(400).json({success:false});
+          res.status(400).json({success:false, msg: error});
      }
   
 };
 // @desc   Get single training
 // @route  GET /api/trainings/:id
 // @access Public
-exports.getTraining = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Get training` });
+exports.getTraining = async(req, res, next) => {
+     try {
+          const training_data = Training.findById(req.id)
+          res.status(200).json({ success: true, msg: "Get training by ID", data: training_data});
+          
+     } catch (error) {
+          res.status(400).json({success:false, msg: error.message});
+     }
 };
 // @desc   Create new training
 // @route  POST /api/trainings
